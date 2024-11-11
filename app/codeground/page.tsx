3bcab0baefd6500dceef75dd3fd4ffb1e832a947
jodyboy <elite.crew66@gmail.com>
2024-11-11 20:09:21 +0700

    feat/testing the new tutorial in codeground

--- app/codeground/page.tsx
+++ app/codeground/page.tsx
@@ -1,20 +1,8 @@
 'use client'
 
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { TransitionWrapper } from "../components";
-import { OpeningContainer } from "../components/openingContainer";
+import StickyCursor from "./stickyCursor";
 
 export default function Home() {
-    const [isTransitioning, setIsTransitioning] = useState(false);
-    const router = useRouter();
-
-    const handleTransition = (route: string) => {
-        setIsTransitioning(true);
-        setTimeout(() => {
-        router.push(route);
-        }, 1300); // Control the transition timing
-    };
 
     return (
         <div 
@@ -23,18 +11,10 @@ export default function Home() {
                 backgroundColor: "var(--background)"
             }}
         >
-            <div className="flex w-full h-full justify-space items-space">
-                <OpeningContainer title="Home" />
-
-                <div className="flex w-full h-full justify-center itmes-center">
-                    <TransitionWrapper isTransitioning={isTransitioning}>
-                        <button 
-                            className="rounded-md p-2 border border-black flex items-center justify-between bg-[#ddd] hover:bg-[#aaa]"
-                            onClick={() => handleTransition('/codeground/pageOne')}
-                        >
-                            Go to Page One
-                        </button>
-                    </ TransitionWrapper>
+            <StickyCursor />
+            <div className="flex w-full h-full justify-center items-center">
+                <div className="w-36 h-36 bg-neutral-300">
+                    Test Image
                 </div>
             </div>
         </div>
