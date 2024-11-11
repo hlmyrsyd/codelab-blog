e02de8667999c3b3fb6d7549029bd3bb9872445d
jodyboy <elite.crew66@gmail.com>
2024-11-11 22:26:30 +0700

    feat/perfecting the bounce and detailing the motion

--- app/codeground/magnetic.tsx
+++ app/codeground/magnetic.tsx
@@ -6,10 +6,10 @@ import Image from "next/image";
 export default function Magnetic() {
     const originX = useMotionValue(0);
     const originY = useMotionValue(0);
-    const smoothOptions = { stiffness: 300, damping: 20, mass: 1.5 };
+    const smoothOptions = { stiffness: 250, damping: 10, mass: 1 };
     const springX = useSpring(originX, smoothOptions);
     const springY = useSpring(originY, smoothOptions);
-    const distanceLimit = 300;
+    const distanceLimit = 200;
 
     const handleMouseMove = (e:any) => {
         const rect = e.currentTarget.getBoundingClientRect();
@@ -20,7 +20,7 @@ export default function Magnetic() {
         const distance = Math.sqrt(distX * distX + distY * distY);
 
         if (distance < distanceLimit) {
-            const pullStrength = 1 - distance / distanceLimit;
+            const pullStrength = 1.3 - distance / distanceLimit;
             originX.set(distX * pullStrength);
             originY.set(distY * pullStrength);
         } else {
@@ -37,6 +37,8 @@ export default function Magnetic() {
     return (
         <motion.div
             className="w-72 h-72 flex justify-center items-center cursor-pointer"
+            initial={{scale:1}}
+            whileHover={{scale:1.2}}
             style={{
                 x: springX,
                 y: springY,
@@ -50,7 +52,7 @@ export default function Magnetic() {
                     alt="contoh gambar"
                     height={600}
                     width={600}
-                    className="object-cover w-full h-full rounded-2xl"                
+                    className="object-cover w-full h-full rounded-[2rem]"                
                 />
             </div>
         </motion.div>
